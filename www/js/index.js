bffb0cea3b1213c37856ae5c10cc42d9ae7960e4
Denchik <Denchik@denchik.denchik>
2014-07-14 16:29:32 +0300

    push

--- www/js/index.js
+++ www/js/index.js
@@ -120,34 +120,64 @@ var app = {
         
         var downloadUrl = "http://www.w3.org/2011/web-apps-ws/papers/Nitobi.pdf";
         var relativeFilePath = "test.pdf";
-        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
-            alert("got filesystem");
-            alert(fileSystem.root.fullPath);
-            alert(fileSystem.root.toURL());
-            alert('/sdcard/DCIM/' + relativeFilePath);
-        
-            var op;
-            op = new FileUploadOptions();
-            
-            op.headers = {
-                Connection: "close"
-            };
-            var fileTransfer = new FileTransfer();
-                fileTransfer.download(
-                        downloadUrl,
-                        '/sdcard/DCIM/' + relativeFilePath,
-                function(entry) {
-                    alert("download complete: " + entry.fullPath);
-                },
-                function(error) {
-                    alert("download error source " + error.source);
-                    alert("download error target " + error.target);
-                    alert("upload error code" + error.code);
-                });
-            }, this.fail);
+        downloadFile(downloadUrl, relativeFilePath);
+        // window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
+            // alert("got filesystem");
+            // alert(fileSystem.root.fullPath);
+            // alert(fileSystem.root.toURL());
+            // alert('/sdcard/DCIM/' + relativeFilePath);
+//         
+            // var op;
+            // op = new FileUploadOptions();
+//             
+            // op.headers = {
+                // Connection: "close"
+            // };
+            // var fileTransfer = new FileTransfer();
+                // fileTransfer.download(
+                        // downloadUrl,
+                        // '/sdcard/DCIM/' + relativeFilePath,
+                // function(entry) {
+                    // alert("download complete: " + entry.fullPath);
+                // },
+                // function(error) {
+                    // alert("download error source " + error.source);
+                    // alert("download error target " + error.target);
+                    // alert("upload error code" + error.code);
+                // });
+            // }, this.fail);
 
     },
     fail:function(error) {
         alert(error.code);
     }
+};
+function downloadFile(fileUrl, fileName) {
+    fileName = fileName.replace(/ /g, '_');
+    fileName = fileName.replace(/\+/g, '_');
+    var remoteFile = encodeURI(fileUrl);
+    var localFileName = fileName;
+    
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
+        alert('1');
+        fileSystem.root.getFile(localFileName, {create: true, exclusive: false}, function(fileEntry) {
+            alert(fileEntry.name);
+            var localPath = fileEntry.fullPath;
+            if (device.platform === "Android" && localPath.indexOf("file://") === 0) {
+                localPath = localPath.substring(7);
+            }
+            alert(remoteFile);
+            alert(localPath);
+            var ft = new FileTransfer();
+            ft.download(remoteFile,
+                localPath, function(entry) {
+                    alert('3');
+                    window.plugins.childBrowser.openExternal('file:///sdcard/'+fileName);
+                    
+                }, fail);
+        }, fail);
+    }, fail);
+};
+function fail(error) {
+        alert(error.code);
 };
