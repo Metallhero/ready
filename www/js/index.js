fc1c1883c56c61b42715fe4943ef4039d6238574
Denchik <Denchik@denchik.denchik>
2014-07-15 14:25:39 +0300

    push

--- www/js/index.js
+++ www/js/index.js
@@ -54,97 +54,98 @@ var app = {
             alert('Initialize'); 
         });
         
-        // var buttomDom;
-        // var statusDom;
-        // var fileSystem;
-//         
-        // window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, 
-        // function(fs) {
-            // fileSystem = fs;
-//     
-            // buttonDom = document.querySelector('#startDl');
-            // buttonDom.addEventListener('touchend', function () {
-//               
-                // buttonDom.setAttribute("disabled","disabled");
-                // var op;
-                // op = new FileUploadOptions();
-//                 
-                // op.headers = {
-                    // Connection: "close"
-                // };
-                // var ft = new FileTransfer();
-                // var uri = encodeURI("http://archive.org/download/Kansas_Joe_Memphis_Minnie-When_Levee_Breaks/Kansas_Joe_and_Memphis_Minnie-When_the_Levee_Breaks.mp3");
-//              
-                // var downloadPath = fileSystem.root.fullPath + "/download.mp3";
-//                 
-                // alert(downloadPath);
-                // alert(uri);
-//                 
-//                 
-                // ft.onprogress = function(progressEvent) {
-// 
-                    // if (progressEvent.lengthComputable) {
-                        // var perc = Math.floor(progressEvent.loaded / progressEvent.total * 100);
-                        // statusDom.innerHTML = perc + "% loaded...";
-                    // } else {
-                        // if(statusDom.innerHTML == "") {
-                            // statusDom.innerHTML = "Loading";
-                        // } else {
-                            // statusDom.innerHTML += ".";
-                        // }
-                    // }
-                // };
-//                                     
-                // ft.download(uri, downloadPath, 
-                // function(entry) {
-                    // statusDom.innerHTML = "";
-                    // alert("download complete: " + entry.fullPath);
-                    // // var media = new Media(entry.fullPath, null, function(e) { alert(JSON.stringify(e));});
-                    // // media.play();
-//                     
-                // }, 
-                // function(error) {
-                    // alert('Crap something went wrong...'+ error.code);  
-                // });
-//               
-//               
-//               
-            // }, false);
-//             
-            // buttonDom.removeAttribute("disabled");
-            // statusDom = document.querySelector('#status');
-//             
-        // }, function(e) {
-            // alert('failed to get fs');
-            // alert(JSON.stringify(e));
-        // });
+        var buttomDom;
+        var statusDom;
+        var fileSystem;
+        
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, 
+        function(fs) {
+            fileSystem = fs;
+    
+            buttonDom = document.querySelector('#startDl');
+            buttonDom.addEventListener('touchend', function () {
+              
+                buttonDom.setAttribute("disabled","disabled");
+                var op;
+                op = new FileUploadOptions();
+                
+                op.headers = {
+                    Connection: "close"
+                };
+                var ft = new FileTransfer();
+                var uri = encodeURI("http://archive.org/download/Kansas_Joe_Memphis_Minnie-When_Levee_Breaks/Kansas_Joe_and_Memphis_Minnie-When_the_Levee_Breaks.mp3");
+             
+                var downloadPath = fileSystem.root.fullPath + "/download.mp3";
+                
+                alert(downloadPath);
+                alert(uri);
+                
+                
+                ft.onprogress = function(progressEvent) {
+
+                    if (progressEvent.lengthComputable) {
+                        var perc = Math.floor(progressEvent.loaded / progressEvent.total * 100);
+                        statusDom.innerHTML = perc + "% loaded...";
+                    } else {
+                        if(statusDom.innerHTML == "") {
+                            statusDom.innerHTML = "Loading";
+                        } else {
+                            statusDom.innerHTML += ".";
+                        }
+                    }
+                };
+                                    
+                ft.download(uri, downloadPath, 
+                function(entry) {
+                    statusDom.innerHTML = "";
+                    alert("download complete: " + entry.fullPath);
+                    var media = new Media(entry.fullPath, null, function(e) { alert(JSON.stringify(e));});
+                    media.play();
+                    
+                }, 
+                function(error) {
+                    alert('Crap something went wrong...'+ error.code);  
+                });
+              
+              
+              
+            }, false);
+            
+            buttonDom.removeAttribute("disabled");
+            statusDom = document.querySelector('#status');
+            
+        }, function(e) {
+            alert('failed to get fs');
+            alert(JSON.stringify(e));
+        });
         
         var downloadUrl = "http://www.w3.org/2011/web-apps-ws/papers/Nitobi.pdf";
         var relativeFilePath = "test.pdf";
         // downloadFile(downloadUrl, relativeFilePath);
-        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
-            alert("got filesystem");
-            alert(fileSystem.root.toURL()+relativeFilePath);
-        
-            var op;
-            op = new FileUploadOptions();
-            
-            op.headers = {
-                Connection: "close"
-            };
-            var fileTransfer = new FileTransfer();
-                fileTransfer.download(
-                        downloadUrl,
-                        fileSystem.root.toURL()+relativeFilePath,
-                function(entry) {
-                    alert("download complete: " + entry.fullPath);
-                },
-                function(error) {
-                    alert("download error source " + error.source);
-                    alert("download error target " + error.target);
-                    alert("upload error code" + error.code);
-                });
-            }, fail);
+        // window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
+            // alert("got filesystem");
+            // alert(fileSystem.root.toURL()+relativeFilePath);
+//         
+            // var op;
+            // op = new FileUploadOptions();
+//             
+            // op.headers = {
+                // Connection: "close"
+            // };
+            // var fileTransfer = new FileTransfer();
+                // fileTransfer.download(
+                        // downloadUrl,
+                        // fileSystem.root.toURL()+relativeFilePath,
+                // function(entry) {
+                    // alert("download complete: " + entry.fullPath);
+                    // showLink(entry.fullPath)
+                // },
+                // function(error) {
+                    // alert("download error source " + error.source);
+                    // alert("download error target " + error.target);
+                    // alert("upload error code" + error.code);
+                // });
+            // }, fail);
             
          
       
